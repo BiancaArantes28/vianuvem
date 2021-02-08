3001b10f230fdf8e68b7de0f452b37895afa7243
BiancaArantes28 <biancaarantes28@gmail.com>
2021-02-07 22:17:36 -0300

    feat: use store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import store from './store';
 import Header from './common/header/Header';
 import HomeContainer from './features/HomeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <div className="container">
-          <Switch>
-            <Route path="/home" component={HomeContainer} />
-            <Redirect to="/home" />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route path="/home" component={HomeContainer} />
+              <Redirect to="/home" />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
