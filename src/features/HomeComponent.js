a61c42663b00f5bb2e84471e9f19786d0ffb6043
BiancaArantes28 <biancaarantes28@gmail.com>
2021-02-07 22:18:20 -0300

    feat: dispatch action and get state

--- src/features/HomeComponent.js
+++ src/features/HomeComponent.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import CardComponent from '../common/card/Card';
 
-const HomeComponent = () => {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginTop: 30,
+    },
+    paper: {
+        height: 140,
+        width: 100,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+}));
+
+const HomeComponent = ({ products }) => {
+    const classes = useStyles();
+
     return (
-        <h1>Home Component</h1>
+        <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+                <Grid container justify="center" spacing={2}>
+                    {products.map((product) => (
+                        <Grid key={product.id} item>
+                            <CardComponent card={product} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
--- src/features/HomeContainer.js
+++ src/features/HomeContainer.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchGetProducts } from '../store/actions/products/ProductsActions';
+
 import HomeComponent from './HomeComponent';
 
 const HomeContainer = () => {
+    const { products } = useSelector((state) => state.products);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchGetProducts());
+    }, [dispatch]);
+
     return (
-        <HomeComponent />
+        <HomeComponent products={products} />
     );
 }
 
