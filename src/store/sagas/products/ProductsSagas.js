2c0135c3734435d3763d2a9784e365655d4ba3ef
BiancaArantes28 <biancaarantes28@gmail.com>
2021-02-15 16:20:09 -0300

    fix: images were changing when state changes

--- src/store/sagas/products/ProductsSagas.js
+++ src/store/sagas/products/ProductsSagas.js
@@ -10,8 +10,24 @@ import { fetchGet } from '../sagas.utils';
 export function* doFetchProducts() {
     try {
         const url = `${getAPIURL()}/api/products/list`;
-        
-        const payload = yield call(fetchGet, url);
+        const imgs = [
+            'https://images-americanas.b2w.io/produtos/01/00/img/1299774/4/1299774456_1GG.jpg',
+            'https://images-americanas.b2w.io/produtos/01/00/img/2575108/6/2575108611_1GG.jpg',
+            'https://images-americanas.b2w.io/produtos/01/00/img/2872269/4/2872269488_1GG.jpg',
+            'https://images-americanas.b2w.io/produtos/01/00/img/1865734/0/1865734054_1GG.jpg',
+            'https://images-americanas.b2w.io/produtos/1836968361/imagens/smartband-xiaomi-miband-5-versao-global/1836968361_1_large.jpg',
+            'https://images-americanas.b2w.io/produtos/01/00/img/2795766/4/2795766480_1GG.jpg',
+        ];
+        const data = yield call(fetchGet, url);
+        const payload = data.map(d => {
+            return {
+                id: d.id,
+                name: d.name,
+                price: d.price,
+                qtd: d.qtd,
+                img: imgs[Math.floor(Math.random() * imgs.length)],
+            };
+        })
         yield put(fetchGetProductsSuccess(payload));
     } catch (error) {
         yield put(fetchGetProductsFailed(error));
